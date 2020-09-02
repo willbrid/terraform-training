14849f0e380aa7cdaf2d57b25a61d43bb3311354
ngaswilly77 <ngaswilly77@gmail.com>
2020-09-02 11:31:43 +0100

    define security group, deploy a basic web server

--- base/main.tf
+++ base/main.tf
@@ -9,8 +9,26 @@ provider "aws" {
 resource "aws_instance" "base" {
   ami = "ami-0083662ba17882949"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
 
   tags = {
     Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
